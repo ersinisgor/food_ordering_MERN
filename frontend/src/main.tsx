d00b5c54feb0253c408d610e27f826527d44e507
Ersin Isgor <ersinisgor@hotmail.com>
2025-01-31 22:15:53 +0300

    add react-query integration; implement user creation on Auth0 login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
-  </StrictMode>
+  </React.StrictMode>
 );
