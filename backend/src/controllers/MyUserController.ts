8fd16db5fce5d18e5990f2283980297b352b3a27
Ersin Isgor <ersinisgor@hotmail.com>
2025-02-01 13:23:37 +0300

    add user update functionality; implement updateCurrentUser method and JWT parsing middleware

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -18,4 +18,26 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { createCurrentUser };
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, city, country } = req.body;
+    const user = await User.findById(req.userId);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
+
+export default { createCurrentUser, updateCurrentUser };
