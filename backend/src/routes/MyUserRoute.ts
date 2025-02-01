76ddf613d3b768e56696d7d712b9d786e3542dd2
Ersin Isgor <ersinisgor@hotmail.com>
2025-02-01 22:36:58 +0300

    add getCurrentUser functionality; implement user retrieval in MyUserController and integrate with frontend

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -6,7 +6,7 @@ import { validateMyUserRequest } from "../middleware/validation";
 const router = express.Router();
 
 // /api/my/user
-
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put(
   "/",
@@ -15,4 +15,5 @@ router.put(
   validateMyUserRequest,
   MyUserController.updateCurrentUser
 );
+
 export default router;
