13f4d8d18ce0f71e3ea3b5fc49e75cf682609992
Ersin Isgor <ersinisgor@hotmail.com>
2025-02-01 22:01:31 +0300

    add user update functionality; implement useUpdateMyUser hook and integrate with UserProfilePage

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,11 +1,9 @@
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-  return (
-    <div>
-      <UserProfileForm />
-    </div>
-  );
+  const { updateUser, isLoading } = useUpdateMyUser();
+  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
 };
 
 export default UserProfilePage;
