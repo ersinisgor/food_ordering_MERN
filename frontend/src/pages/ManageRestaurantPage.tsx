31a23e7e4d5eca191c928526b5b16ca57e7c783c
Ersin Isgor <ersinisgor@hotmail.com>
2025-02-03 22:07:57 +0300

    add restaurant creation functionality; integrate useCreateMyRestaurant hook in ManageRestaurantPage

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,7 +1,11 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
 };
 
 export default ManageRestaurantPage;
