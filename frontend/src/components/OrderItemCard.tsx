400b9f18d1d28dfb4cf76e86bd7fd8cf4b1051f4
Ersin Isgor <ersinisgor@hotmail.com>
2025-02-17 22:40:57 +0300

    add OrderItemCard component and Tabs for managing restaurant orders; implement order display and navigation

---
+++ frontend/src/components/OrderItemCard.tsx
@@ -0,0 +1,54 @@
+import { Order } from "@/types";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+import { Separator } from "./ui/separator";
+
+type Props = {
+  order: Order;
+};
+
+const OrderItemCard = ({ order }: Props) => {
+  const getTime = () => {
+    const orderDateTime = new Date(order.createdAt);
+
+    const hours = orderDateTime.getHours();
+    const minutes = orderDateTime.getMinutes();
+
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${hours}:${paddedMinutes}`;
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="grid md:grid-cols-4 gap-4 justify-between mb-3">
+          <div>
+            Customer Name:
+            <span className="ml-2 font-normal">
+              {order.deliveryDetails.name}
+            </span>
+          </div>
+          <div>
+            Delivery address:
+            <span className="ml-2 font-normal">
+              {order.deliveryDetails.addressLine1}, {order.deliveryDetails.city}
+            </span>
+          </div>
+          <div>
+            Time:
+            <span className="ml-2 font-normal">{getTime()}</span>
+          </div>
+          <div>
+            Total Cost:
+            <span className="ml-2 font-normal">
+              ${(order.totalAmount / 100).toFixed(2)}
+            </span>
+          </div>
+        </CardTitle>
+        <Separator />
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default OrderItemCard;
