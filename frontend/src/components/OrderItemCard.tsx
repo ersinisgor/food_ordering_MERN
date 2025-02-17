2394699576af84b4c27c50cc5d4b0b08c47a8851
Ersin Isgor <ersinisgor@hotmail.com>
2025-02-17 22:48:03 +0300

    add Select component for order status selection in OrderItemCard; enhance order display with badges

--- frontend/src/components/OrderItemCard.tsx
+++ frontend/src/components/OrderItemCard.tsx
@@ -1,6 +1,16 @@
 import { Order } from "@/types";
-import { Card, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Separator } from "./ui/separator";
+import { Badge } from "./ui/badge";
+import { Label } from "./ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { ORDER_STATUS } from "@/config/order-status-config";
 
 type Props = {
   order: Order;
@@ -47,6 +57,31 @@ const OrderItemCard = ({ order }: Props) => {
         </CardTitle>
         <Separator />
       </CardHeader>
+      <CardContent className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+          {order.cartItems.map(cartItem => (
+            <span>
+              <Badge variant="outline" className="mr-2">
+                {cartItem.quantity}
+              </Badge>
+              {cartItem.name}
+            </span>
+          ))}
+        </div>
+        <div className="flex flex-col space-y-1.5">
+          <Label htmlFor="status">What is the status of this order?</Label>
+          <Select>
+            <SelectTrigger id="status">
+              <SelectValue placeholder="Status" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              {ORDER_STATUS.map(status => (
+                <SelectItem value={status.value}>{status.label}</SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </CardContent>
     </Card>
   );
 };
